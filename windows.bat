8071e17b6cb0492b78960451030444f02bea4061
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2017-08-19 12:02:50 -0400

    more windows things

--- windows.bat
+++ windows.bat
@@ -1,41 +1,70 @@
+REM Install Chocolatey package manager
+Set-ExecutionPolicy unrestricted
+Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+REM Enables Windows Linux Subsystem (Bash)
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
+
+REM Disables Internet Explorer (I hope)
+dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
+
+REM Show file extensions
+reg add HKCUSoftwareMicrosoftWindowsCurrentVersionExplorerAdvanced /v HideFileExt /t REG_DWORD /d 0 /f
+
 REM GUI Programs
-choco install googlechrome -y
-choco install winrar -y
-choco install firefox -y
-choco install 7zip.install -y
-choco install steam -y
-choco install skype -y
-choco install visualstudiocode -y
-choco install telegram -y
-choco install ccleaner -y
-choco install malwarebytes -y
-choco install atom -y
-choco install virtualbox -y
-choco install dropbox -y
-choco install teamviewer -y
-choco install itunes -y
-choco install gimp -y
-choco install calibre -y
-choco install unity -y
-choco install cyberduck.install -y
-choco install whatsapp -y
-choco install slack -y
-choco install f.lux -y
-choco install postman -y
-choco install robo3t.install -y
-choco install etcher -y
-choco install obs-studio -y
-choco install arduino -y
-choco install handbrake -y
-choco install velocity -y
-choco install google-backup-and-sync -y
+choco install -y googlechrome
+choco install -y winrar
+choco install -y firefox
+choco install -y 7zip.install
+choco install -y steam
+choco install -y skype
+choco install -y visualstudiocode
+choco install -y telegram
+choco install -y ccleaner
+choco install -y malwarebytes
+choco install -y atom
+choco install -y virtualbox
+choco install -y dropbox
+choco install -y windirstat
+choco install -y teamviewer
+choco install -y itunes
+choco install -y gimp
+choco install -y calibre
+choco install -y unity
+choco install -y cyberduck.install
+choco install -y whatsapp
+choco install -y slack
+choco install -y f.lux
+choco install -y postman
+choco install -y robo3t.install
+choco install -y etcher
+choco install -y obs-studio
+choco install -y arduino
+choco install -y handbrake
+choco install -y velocity
+choco install -y google-backup-and-sync
 
 REM CLI tools
-choco install python3 -y
-choco install youtube-dl -y
-choco install git.install -y
-choco install neovim -y
-choco install curl -y
-choco install wget -y
-choco install mongodb -y
-choco install nodejs.install -y
+choco install -y python3
+choco install -y youtube-dl
+choco install -y git.install
+choco install -y neovim
+choco install -y php
+choco install -y curl
+choco install -y wget
+choco install -y mysql
+choco install -y mongodb
+choco install -y nodejs.install
+
+REM Node.js global packages
+npm install -g gulp csvtojson js-beautify typescript
+
+REM Visual Studio Code Extensions
+code --install-extension eg2.tslint
+
+REM Disable UAC Prompt
+C:\Windows\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+REM Run Windows Update
+wuauclt.exe /updatenow
