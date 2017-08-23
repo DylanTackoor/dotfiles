5b0cf451a451cd6837abcf5fa03bbf4df9186b1b
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2017-08-23 09:54:37 -0400

    mac: prompt for reboot at end

--- mac.sh
+++ mac.sh
@@ -544,3 +544,22 @@ docker -v
 echo "Typescript:"
 tsc -v
 # mongod --version
+
+function reboot() {
+  read -p "Do you want to reboot your computer now? (y/N)" choice
+  case "$choice" in
+    y | Yes | yes ) echo "Yes"; exit;; # If y | yes, reboot
+    n | N | No | no) echo "No"; exit;; # If n | no, exit
+    * ) echo "Invalid answer. Enter \"y/yes\" or \"N/no\"" && return;;
+  esac
+}
+
+# Call on the function
+if [[ "Yes" == $(reboot) ]]
+then
+  echo "Rebooting."
+  sudo reboot
+  exit 0
+else
+  exit 1
+fi
