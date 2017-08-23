c44bd4e09e9aa11409b86e2070a335eaa3a3201a
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2017-08-23 17:37:00 -0400

    win: new apps + even more reorg

--- windows.bat
+++ windows.bat
@@ -12,46 +12,8 @@ dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64
 REM Show file extensions
 reg add HKCUSoftwareMicrosoftWindowsCurrentVersionExplorerAdvanced /v HideFileExt /t REG_DWORD /d 0 /f
 
-REM GUI Programs
-choco install -y googlechrome
-choco install -y winrar
-choco install -y firefox
-choco install -y 7zip.install
-choco install -y steam
-choco install -y skype
-choco install -y telegram
-choco install -y ccleaner
-choco install -y malwarebytes
-choco install -y atom
-choco install -y dropbox
-choco install -y windirstat
-choco install -y teamviewer
-choco install -y itunes
-choco install -y gimp
-choco install -y calibre
-choco install -y spotify
-choco install -y whatsapp
-choco install -y slack
-choco install -y f.lux
-choco install -y obs-studio
-choco install -y handbrake
-choco install -y google-backup-and-sync
-
-REM Dev Tools
-choco install -y docker-for-windows
-choco install -y visualstudiocode
-choco install -y virtualbox
-choco install -y unity
-choco install -y cyberduck.install
-choco install -y postman
-choco install -y robo3t.install
-choco install -y etcher
-choco install -y arduino
-choco install -y velocity
-choco install -y webstorm
-choco install -y pycharm-community
-
 REM CLI tools
+choco install -y gow
 choco install -y python3
 choco install -y youtube-dl
 choco install -y git.install
@@ -62,6 +24,66 @@ choco install -y wget
 choco install -y mysql
 choco install -y mongodb
 choco install -y nodejs.install
+
+REM Browsers
+choco install -y googlechrome
+choco install -y firefox
+
+REM Archive Utilities
+choco install -y winrar
+choco install -y 7zip.install
+
+REM Messaging
+choco install -y skype
+choco install -y telegram
+choco install -y whatsapp
+choco install -y slack
+
+REM Cloud Storage
+choco install -y dropbox
+choco install -y google-backup-and-sync
+
+REM Personal Library
+choco install -y itunes
+choco install -y calibre
+choco install -y plexmediaserver
+choco install -y spotify
+
+REM Games
+choco install -y steam
+choco install -y minecraft
+
+REM Utilities
+choco install -y ccleaner
+choco install -y gimp
+choco install -y malwarebytes
+choco install -y windirstat
+choco install -y teamviewer
+choco install -y f.lux
+choco install -y obs-studio
+choco install -y handbrake
+
+REM Dev Tools
+choco install -y visualstudiocode
+choco install -y atom
+choco install -y unity
+choco install -y arduino
+choco install -y jetbrainstoolbox
+choco install -y webstorm
+choco install -y pycharm-community
+choco install -y phpstorm
+
+choco install -y velocity
+
+choco install -y cyberduck.install
+
+choco install -y postman
+choco install -y robo3t.install
+choco install -y mysql.workbench
+
+choco install -y virtualbox
+choco install -y docker-toolbox
+choco install -y etcher
 
 REM Node.js global packages
 npm install -g gulp csvtojson js-beautify typescript
