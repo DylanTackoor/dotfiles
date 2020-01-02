8eab7162d2f50e8dd839b581ef38b4468a81b4b6
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2020-01-02 10:50:35 -0500

    ubuntu: reboot prompt

--- ubuntu.sh
+++ ubuntu.sh
@@ -229,7 +229,22 @@ node -v
 npm -v
 tsc -v
 
-# # TODO: Reboot after Enter
-# sudo reboot
+# Prompt for reboot
+function reboot() {
+  read -p "Do you want to reboot your computer now? (y/N)" choice
+  case "$choice" in
+    y | Yes | yes ) echo "Yes"; exit;; # If y | yes, reboot
+    n | N | No | no) echo "No"; exit;; # If n | no, exit
+    * ) echo "Invalid answer. Enter \"y/yes\" or \"N/no\"" && return;;
+  esac
+}
 
-# xserver-xorg-input-synaptics
+# Call on the function
+if [[ "Yes" == $(reboot) ]]
+then
+  echo "Rebooting."
+  sudo reboot
+  exit 0
+else
+  exit 1
+fi
