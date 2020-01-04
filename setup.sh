32b8121e9fc35f85a9f27ec71d9bd96e168d825a
Dylan Tackoor <mynameisdylantackoor@gmail.com>
2020-01-04 10:06:09 -0500

    setup executes local or remote scripts

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
+system="$(uname -s)"
+case "${system}" in
     Linux*)     script=ubuntu.sh;;
     Darwin*)    script=mac.sh;;
-    *)          script="UNKNOWN:${unameOut}"
+    *)          script="UNKNOWN:${system}"
 esac
-
-echo ${script}
 
 # TODO: if machine includes UNKNOWN, log error and exit 1
 
-# if ~/.dotfiles exists
-	# run local script
-# else
-	# curl script and pipe to bash
+if [ -d ~/.dotfiles ]; then
+    echo "Detected ${system}, using local ${script}"
+    ./"${script}"
+else
+    echo "Detected ${system}, downloading remote ${script}"
+    wget -q -O - "https://raw.githubusercontent.com/DylanTackoor/dotfiles/master/${script}" | bash
+fi
